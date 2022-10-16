d15dd7a44127cd3772af2f6d51d290941335c851
Dhiraj Jadhav <dhiraj-jadhav@outlook.com>
2022-10-16 22:15:15 +0530

    Demo program added

---
+++ Demo/src/Example.java
@@ -0,0 +1,10 @@
+
+public class Example 
+{
+	int c=30;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
---
+++ Demo/src/Example2.java
@@ -0,0 +1,9 @@
+
+public class Example2 
+{
+	public static void main(String[]args)
+	{
+		Example example=new Example();
+		System.out.println("the value of c is>>>>"+example.c);
+	}
+}
